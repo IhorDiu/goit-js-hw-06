91113e18285663060f871ee0a30b93fd7f2d9b43
IhorDiu <110743288+IhorDiu@users.noreply.github.com>
2022-12-10 23:38:13 +0200

    Task_9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,23 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+
+const ref = {
+  bgColor: document.querySelector('.color'),
+  widgetBtn: document.querySelector('.change-color'),
+  body: document.querySelector('body'),
+};
+
+ref.widgetBtn.addEventListener('click', onBtnClick);
+
+function onBtnClick() {
+  let changeBgColor = getRandomHexColor();
+  ref.bgColor.innerHTML = changeBgColor;
+  ref.body.style.background = changeBgColor;
 }
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+
+
