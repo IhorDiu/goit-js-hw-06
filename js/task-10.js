def011f784e2a2a004252aae67012e75e2362300
IhorDiu <110743288+IhorDiu@users.noreply.github.com>
2022-12-11 16:37:11 +0200

    Task_10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,40 @@
+
+const ref = {
+  counterValue: document.querySelector('#controls input'),
+  createBoxesBtn: document.querySelector('[data-create]'),
+  destroyBoxesBtn: document.querySelector('[data-destroy]'),
+  boxesMarkup: document.querySelector('#boxes'),
+};
+
+ref.createBoxesBtn.addEventListener('click', onCreateBoxesBtn);
+ref.destroyBoxesBtn.addEventListener('click', onDestroyBoxesBtn);
+
+
+
+function createBoxes(amount) {
+  let makeup = '';
+  let step = 0;
+  for (let i = 0; i < amount; i += 1) { 
+  step = 30 + 10 * i;
+  makeup += `<div style="width:${step}px; height:${step}px; 
+  background-color: ${getRandomHexColor()}"></div>`;
+  }
+  return makeup;
+};
+
+console.log("getRandomHexColor()", getRandomHexColor())
+
+ function onCreateBoxesBtn() {
+ const counter = ref.counterValue.valueAsNumber;
+ ref.boxesMarkup.insertAdjacentHTML('beforeend', createBoxes(counter));
+};
+
+function onDestroyBoxesBtn() {
+ref.counterValue.value = '';
+ref.boxesMarkup.innerHTML = '';
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
